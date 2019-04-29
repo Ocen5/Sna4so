92d495bed81cd66a35019b87c62781c9d2f0b631
michel-98 <m.antonacci25@studenti.uniba.it>
2019-04-29 15:28:26 +0200

    Aggiornata la presa degli input e passaggio dei parametri

--- src/main/java/it/uniba/sotorrent/SOQueryAnswerDay.java
+++ src/main/java/it/uniba/sotorrent/SOQueryAnswerDay.java
@@ -10,28 +10,48 @@ import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.QueryParameterValue;
 
-/*
+/**
  * Class for run query from answers table with year, month, day, limit.
  */
 public final class SOQueryAnswerDay extends ASOQuery implements ISOQuery {
 
+	/*
+	 * Valid parameters for SOQueryAnswerDay.
+	 */
+
 	/**
-	 * Default constructor, see ASOQuery constructor.
+	 * yyyy 	The year for the WHERE clause of the query.
+	 * mm 		The month for the WHERE clause of the query.
+	 * dd 		The day for the WHERE clause of the query.
+	 * limit 	The limit of tuples.
+	 */
+	private Integer yyyy, mm, dd, limit;
+
+	/**
+	 * Default constructor, init variables of the query and
+	 * see ASOQuery constructor for instance of BigQuery API service.
+	 * @param year	The year parameter to be inserted in the query.
+	 * @param month	The month parameter to be inserted in the query.
+	 * @param day 	The day parameter to be inserted in the query.
+	 * @param lim 	The limit parameter to be inserted in the query.
+	 * 
 	 * @throws FileNotFoundException See stack trace for proper location.
 	 * @throws IOException  See stack trace for proper location.
 	**/
-	public SOQueryAnswerDay() throws FileNotFoundException, IOException {
+	public SOQueryAnswerDay(final Integer year, final Integer month, final Integer day, final Integer lim)
+			throws FileNotFoundException, IOException {
+
+		super();	//ASOQuery constructor.
+		//Init variables
+		yyyy = year;
+		mm = month;
+		dd = day;
+		limit = lim;
 
 	}
 
 	@Override
-	public Job runQuery(final String[] values) throws InterruptedException {
-
-		//Variabili per la query
-		Integer yyyy = Integer.valueOf(values[0]),
-				mm = Integer.valueOf(values[1]),
-				dd = Integer.valueOf(values[2]),
-				limit = Integer.valueOf(values[4]);
+	public Job runQuery() throws InterruptedException {
 
 		// Use standard SQL syntax for queries.
 		// See: https://cloud.google.com/bigquery/sql-reference/
